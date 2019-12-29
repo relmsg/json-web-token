52132b5c6b5fbccdf7a4964f2ff46e70511901a4
Oleg Kozlov <h1karo@outlook.com>
2019-12-29 22:44:45 +0300

    Add uniqid identifier generator implementation

---
+++ src/Storage/UniqIdIdentifierGenerator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is a part of Relations Messenger Json Web Token Implementation.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://gitlab.com/relmsg/json-web-token
+ * @link      https://dev.relmsg.ru/packages/json-web-token
+ * @copyright Copyright (c) 2018-2020 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/json-web-token
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Security\Jwt\Storage;
+
+/**
+ * Class UniqIdIdentifierGenerator provides generation via function { @see uniqid() }.
+ *
+ * @package RM\Security\Jwt\Storage
+ * @author  h1karo <h1karo@outlook.com>
+ */
+class UniqIdIdentifierGenerator implements IdentifierGeneratorInterface
+{
+    public string $prefix = '';
+
+    public function generate(): string
+    {
+        return uniqid($this->prefix, true);
+    }
+}
