c79049860115b8a9a4c73a4e950e60a3840a0b08
Oleg Kozlov <h1karo@outlook.com>
2019-12-29 22:43:53 +0300

    Update payload claim descriptions

--- src/Token/Payload.php
+++ src/Token/Payload.php
@@ -17,7 +17,10 @@
 namespace RM\Security\Jwt\Token;
 
 use RM\Security\Jwt\Handler\ExpirationClaimHandler;
+use RM\Security\Jwt\Handler\IssuedAtClaimHandler;
 use RM\Security\Jwt\Handler\IssuerClaimHandler;
+use RM\Security\Jwt\Handler\NotBeforeClaimHandler;
+use RM\Security\Jwt\Service\TokenServiceInterface;
 use RM\Security\Jwt\Storage\TokenStorageInterface;
 
 /**
@@ -31,32 +34,43 @@ class Payload extends ClaimCollection
     /**
      * Issuer is a unique identity of token generator server, authentication server or security server.
      * You can set this claim to check where token generated.
-     * It is maybe helps you if you use several servers with own token id { @see Payload::CLAIM_ID } cache server { @see TokenStorageInterface }.
+     * It is maybe helps you if you use several servers with own token id { @see Payload::CLAIM_IDENTIFIER } cache server { @see TokenStorageInterface }.
      * We recommend to set this claim.
-     * You can just use { @see IssuerClaimHandler } to manage this claim.
+     *
+     * @see IssuerClaimHandler The manager for this claim.
      */
     public const CLAIM_ISSUER = 'iss';
     /**
      * Subject is a unique identity of application who wants to get access to the audience { @see Payload::CLAIM_AUDIENCE }.
      * It is required claim.
+     * No handler for this claim because { @see TokenServiceInterface } processes it directly.
      */
     public const CLAIM_SUBJECT = 'sub';
     /**
      * Audience is a unique identity of object token provides access to.
-     * It is required claim. May have same value as { @see Payload::CLAIM_SUBJECT }.
+     * It is required claim. May have same value as { @see Payload::CLAIM_SUBJECT } claim.
+     * No handler for this claim because { @see TokenServiceInterface } processes it directly.
      */
     public const CLAIM_AUDIENCE = 'aud';
     /**
      * Expiration is a time in UNIX format when token expires.
      * It is required claim.
-     * You can just use { @see ExpirationClaimHandler } to manage this claim.
+     *
+     * @see ExpirationClaimHandler The manager for this claim.
      */
     public const CLAIM_EXPIRATION = 'exp';
     /**
      * Not before time is a time in UNIX format before which the token is not valid.
      *
+     * @see NotBeforeClaimHandler The manager for this claim.
      */
     public const CLAIM_NOT_BEFORE = 'nbf';
-    public const CLAIM_ISSUED_AT  = 'iat';
-    public const CLAIM_ID         = 'jti';
+    /**
+     * Issued at time is a time in UNIX format of token creation.
+     * Often a value of this claim equals a value of { @see CLAIM_NOT_BEFORE } claim.
+     *
+     * @see IssuedAtClaimHandler The manager for this claim.
+     */
+    public const CLAIM_ISSUED_AT = 'iat';
+    public const CLAIM_ID        = 'jti';
 }
